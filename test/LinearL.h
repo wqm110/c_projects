3a9d83ed1296bb291925d2e5b6b7c5598cea0ce5
wqm110 <1003053968@qq.com>
2020-09-07 19:57:01 +0800

    线性表的连续存储、链式存储部分

--- test/LinearL.h
+++ test/LinearL.h
@@ -13,6 +13,7 @@ typedef struct node {
 /*初始化链表*/
 LinkList InitSeqLinkList() {
 //    建立一个空链表
+    printf("建立一个空链表。。");
     LinkList head;                  //头指针
     head = malloc(sizeof(Node));    //动态构建一个节点，他是头结点
     head->next = NULL;
@@ -35,4 +36,38 @@ int LengthLinkList(LinkList head) {
     printf("共发现%d个元素。bye....", cnt);
     return cnt;
 
+}
+
+/*插入*/
+LinkList InsertLinkList(LinkList LL, int x) {
+
+}
+
+/*读取数据*/
+Node *GetLinklist(LinkList head, int i) {
+    //在单链表head中查找第i个元素节点。
+    //若找到返回该节点的指针
+    //若没找到返回 Null
+    printf("查找第%d个元素", i);
+    Node *p;//工作指针
+    p = head->next;//出始 p 指向 首节点
+    int c = 1;
+    while ((c < i) && (p != NULL)) {
+        p = p->next;
+        c++;
+    }
+    if (i == c) return p;
+    else return NULL;
+}
+
+/*定位*/
+int LocateLinkList(LinkList head, DataType x) {
+    Node *p = head;
+    p = p->next;
+    int loc = 0;
+    while ((p != NULL) && (p->data.num != x.num)) {
+        loc++;
+    }
+    if (p != NULL) return loc + 1;
+    else return 0;
 }
--- test/main.c
+++ test/main.c
@@ -6,15 +6,16 @@
 #include "Linear.h"
 #include "LinearL.h"
 
-void list_continue_save() {
+void list_link_save(DataType stu, DataType stu2, DataType stu3) {
+    //线性表的链式存储
+
+    LinkList LL = InitSeqLinkList();//建链
+
+}
+
+void list_continue_save(DataType stu, DataType stu1, DataType stu2, DataType stu3, DataType stu4, DataType stu5,
+                        DataType stu6, DataType stu7, DataType stu8) {
     //线性表的连续存储
-    DataType stu, stu1, stu2, stu3, stu4, stu5, stu6, stu7, stu8;
-    stu.num = 19;
-    stu1.num = 20;
-    stu3.num = 17;
-    stu2.num, stu4.num = 21;
-    stu5.num, stu6.num = 22;
-    stu7.num, stu8.num = 23;
 
     L = InsertSeqList(L, stu, 1);
     L = InsertSeqList(L, stu1, 2);
@@ -23,13 +24,25 @@ void list_continue_save() {
     L = InsertSeqList(L, stu4, 5);
     L = InsertSeqList(L, stu5, 6);
     L = InsertSeqList(L, stu6, 7);
-    LocateSeqlist(L, stu4);
+    L = InsertSeqList(L, stu7, 7);
+    L = InsertSeqList(L, stu8, 7);
+    DeleteSeqlist(L, 8);
+    LocateSeqlist(L, stu7);
 }
 
 int main() {
-
-    list_continue_save();
+    DataType stu, stu1, stu2, stu3, stu4, stu5, stu6, stu7, stu8;
+    stu.num = 19;
+    stu1.num = 20;
+    stu3.num = 17;
+    stu2.num, stu4.num = 21;
+    stu5.num, stu6.num = 22;
+    stu7.num, stu8.num = 23;
+//    list_continue_save(stu, stu1, stu2, stu3, stu4, stu5, stu6, stu7, stu8);
     //线性表的 链式存储
+    list_link_save(stu, stu1, stu2);
     return 0;
 }
+
+
 
